d4b62f52c28d5d3312773dec06b3a8b163159aa7
zmeu` <tsraduu@gmail.com>
2023-10-16 21:26:32 +0000

    Modified two functions
    Now the user has the ability to select the colour and draw
    
    The initial colour is a shade of gray that works if the user does not select any colour

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,7 @@ const colour = document.getElementById("colour");
 const colourCircle = document.getElementById("colour-circle");
 
 let isMouseDown = false;
-const selectedColor = colour.value;
+let selectedColor = colour.value;
 
 //< Functions
 
@@ -22,19 +22,20 @@ function updateGameBoard() {
   for (let i = 0; i < squares * squares; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
-    square.style.border = `1px solid black`;
 
     //* event listeners for mouseover and mousedown
     //> As the event listeners are added inside the for loop, each square that's added to the game board gets all these event listeners
 
     square.addEventListener("mousedown", () => {
       square.classList.add("hov-square");
+      square.style.backgroundColor = selectedColor;
       isMouseDown = true;
     });
 
     square.addEventListener("mouseover", () => {
       if (isMouseDown) {
         square.classList.add("hov-square");
+        square.style.backgroundColor = selectedColor;
       }
     });
 
@@ -86,5 +87,7 @@ colourCircle.addEventListener("click", handleColourCircleClick);
 // colour.addEventListener("change", handleColourChange);
 colour.addEventListener("change", function () {
   // const selectedColor = colour.value;
-  colorCircle.style.backgroundColor = selectedColor;
+  selectedColor = this.value;
+  colourCircle.style.backgroundColor = selectedColor;
+  console.log(selectedColor);
 });
